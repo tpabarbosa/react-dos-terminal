a8449153cb2a6e205151af3a88650d534243b3f4
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-03-04 11:21:18 -0300

    feat: add more methods to public useTerminal

--- src/hooks/useTerminal.ts
+++ src/hooks/useTerminal.ts
@@ -1,12 +1,21 @@
+import { TerminalColors } from '../components/Terminal'
 import { useCommand } from '../contexts/CommandContext'
+import { FakeFileSystem, useFileSystem } from '../contexts/FileSystemContext'
 import { useTerminalInternal } from '../contexts/TerminalContext'
 
 export const useTerminal = () => {
     const terminal = useTerminalInternal()
     const command = useCommand()
+    const fileSystem = useFileSystem()
 
     return {
         output: terminal.output,
+        setColors: (value: TerminalColors) =>
+            terminal.setConfig({ config: 'setColors', value }),
+        setPrompt: (value: string) =>
+            terminal.setConfig({ config: 'setPrompt', value }),
+        setFiles: (files: FakeFileSystem) => fileSystem.setFiles(files),
+        setCurrentDir: (value: string) => fileSystem.setCurrentDir(value),
         isRunningCommand: command.isRunningCommand,
         endRunningCommand: command.endRunningCommand,
     }
