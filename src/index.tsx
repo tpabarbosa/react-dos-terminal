aa65b3a2f69029820558061bdbae634c10fb3ba7
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-03-04 17:53:11 -0300

    fix: update exports on package entry point

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,17 @@ import { CommandScreen } from './components/CommandScreen'
 import Input from './components/Input'
 import Output from './components/Output'
 import { useInput } from './hooks/useInput'
-import { Command, FakeCommand, CommandProps } from './contexts/CommandContext'
+import {
+    Command,
+    FakeCommand,
+    CommandProps,
+    CommandToConfigTerminal,
+    CommandToOutput,
+} from './contexts/CommandContext'
 import { useStateMachine, Machine } from './hooks/machines/useStateMachine'
 import colorsHelper from './helpers/colors'
 import commandsHelper from './helpers/commands'
-import { FakeFile } from './contexts/FileSystemContext'
+import { FakeFile, FakeFileCommand } from './contexts/FileSystemContext'
 import { useOutputHandler } from './hooks/useOutputHandler'
 import { ExcludeCommandsOptions, TerminalLoadingScreenOptions } from './config'
 import { useTerminal } from './hooks/useTerminal'
@@ -32,10 +38,13 @@ export {
 export type {
     Command,
     CommandProps,
+    CommandToConfigTerminal,
+    CommandToOutput,
     Machine,
     TerminalColors,
     FakeCommand,
     FakeFile,
+    FakeFileCommand,
     TerminalLoadingScreenOptions,
     ExcludeCommandsOptions,
 }
